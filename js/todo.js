bb66ac8ead1e8ed37d952f804a9899a75fd7b298
buckethead1986 <rspeckpfs@gmail.com>
2017-11-05 14:47:10 -0500

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,26 @@
-$(document).ready(function(){
-  // call functions here
+$(document).ready(function() {
+  addItemToList();
 });
 
-// define functions here
+function addItemToList() {
+  $("form").on("submit", function(event) {
+    var listItem = $("#item").val();
+    $("ol").append("<li>" + listItem + "</li>");
+    event.preventDefault();
+  });
+}
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   javascriptAddItemToList();
+// });
+//
+// javascriptAddItemToList = ev => {
+//   const jsList = document.getElementsByTagName("ol")[0];
+//   document.getElementById("form").addEventListener("submit", ev => {
+//     ev.preventDefault();
+//     const item = document.getElementById("item").value;
+//     const newList = document.createElement("li");
+//     newList.innerText = item;
+//     jsList.appendChild(newList);
+//   });
+// };
